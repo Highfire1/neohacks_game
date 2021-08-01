c7c9e42630395bee9e64c98f943a1e5ef1899782
Highfire1 <theodduni@gmail.com>
2021-08-01 14:54:05 -0700

    better (?) dialog system

--- js/dialogue.js
+++ js/dialogue.js
@@ -1,8 +1,49 @@
 let strings = {
-    "welcome" : ""
+    1 : [
+        ["???", "Once upon a time in a far away land,"],
+        ["???", "There lived a witch named Celestina."],
+        ["???", "She lived with her pet bat, Ambrose."],
+        ["???", "And she brewed potions for a living."],
+        ["???", "One day as she was brewing a potion, she didn't realize that the window was open and a magical wind current came through the door."],
+        ["???", "Ambrose was whisked away by the wind"],
+        ["???", "Celestina turned around to find that Ambrose was missing."],
+    ]
+} 
+
+// this will be replaced later
+let dialogue = 1
+
+let dialoguenum = 0
+
+// unused currently
+function pretty_dialogue() {  
+    console.log("doing dialogue")
+
+    // todo : read wanted dialog from localStorage
+
+    const dianode = document.createElement("dialog") 
+    const pnode = document.createElement("p")
+    const qnode = document.createElement("q")
+    
+    console.log(strings[dialogue][dialoguenum][1])
+    qnode.textContent = strings[dialogue][dialoguenum][1]
+
+    pnode.appendChild(qnode)
+    dianode.appendChild(qnode)
+
+    dianode.setAttribute("open", true)
+    dianode.setAttribute("id", "dialogue")
+    dianode.addEventListener("click", function(){edit_dialogue()} )
+
+    document.currentScript.parentNode.appendChild(dianode)
+
+    dialoguenum += 1
 }
 
+function editDialogue() {
+    console.log(document.getElementById("dialogue"))
 
-function pretty_dialogue(string) {
+    document.getElementById("dialogue").textContent = strings[dialogue][dialoguenum][1]
     
+    dialoguenum += 1
 }
