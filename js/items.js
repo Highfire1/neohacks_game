40cfa836e4f7073fd95808daa90b36c92faea170
Highfire1 <theodduni@gmail.com>
2021-07-31 17:40:27 -0700

    basic js framework set up

---
+++ js/items.js
@@ -0,0 +1,18 @@
+
+let items = {
+
+    "eyeOfNewt" : {
+        name : "Eye of Newt",
+        image : "newt.png",
+        count : 0,
+    },
+
+    "batWing" : {
+        name : "Wing of Bat",
+        image : "batwing.png",
+        count : 0
+    }
+
+  };
+  
+//export {items};
--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
+console.log(items.batWing.name)
+
 let d = new Date();
 document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+
+document.body.innerHTML += "<p>batwing name = " + items.batWing.name + "</p>"
+
