1d86b75b0548d191fce0dae6607ab0e25baa73b9
Highfire1 <theodduni@gmail.com>
2021-07-31 20:22:05 -0700

    yeet useless js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
-
-
-document.body.innerHTML += "<p>batwing name = " + items.batWing.name + "</p>"
 console.log(items.batWing.name) 
 
 let cauldron = []
