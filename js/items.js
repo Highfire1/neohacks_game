d0af43d52b74ad75c8e8818ab5e9c373aaa7beea
Highfire1 <theodduni@gmail.com>
2021-07-31 18:46:29 -0700

    basic framework for recipes

--- js/items.js
+++ js/items.js
@@ -1,4 +1,3 @@
-
 let items = {
 
     "eyeOfNewt" : {
@@ -11,8 +10,40 @@ let items = {
         name : "Wing of Bat",
         image : "batwing.png",
         count : 0
+    },
+
+    "magicFuel" : {
+        name : "Magic Fuel",
+        image : "",
+        count : 0
     }
 
   };
-  
-//export {items};
+
+let recipes = {
+    "magicFuel" : [items.eyeOfNewt, items.batWing]
+}
+
+function lookForRecipe(cauldron_array) {
+
+    // if we don't want order to matter, add a .sort() around here
+    // if we want to add some time-based mechanic, this will need some rework
+
+    for (var key in recipes) {
+        console.log(key)              // returns magicFuel
+        console.log(cauldron_array)   // returns an array
+
+        if (arraysEqual(recipes.key, cauldron_array)) {
+
+            console.log("RECIPE DETECTED")
+            break;
+        }
+    }
+    console.log("no recipe")
+}
+
+// from stackoverflow
+function arraysEqual(a1,a2) {
+    /* WARNING: arrays must not contain {objects} or behavior may be undefined */
+    return JSON.stringify(a1)==JSON.stringify(a2);
+}
--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-console.log(items.batWing.name)
-
 let d = new Date();
 document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
 
-document.body.innerHTML += "<p>batwing name = " + items.batWing.name + "</p>"
 
+document.body.innerHTML += "<p>batwing name = " + items.batWing.name + "</p>"
+console.log(items.batWing.name) 
+
+let cauldron = []
+
+cauldron.push(items.eyeOfNewt)
+
+lookForRecipe(cauldron);
+
+cauldron.push(items.batWing)
+
+lookForRecipe(cauldron);
