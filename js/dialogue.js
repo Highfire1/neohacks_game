6d260f9a40acef446de3c96dc2552fce1e34063b
Highfire1 <theodduni@gmail.com>
2021-08-01 17:07:04 -0700

    dialogue

--- js/dialogue.js
+++ js/dialogue.js
@@ -1,13 +1,76 @@
 let strings = {
+    // intro sequence
     1 : [
         ["???", "Once upon a time in a far away land,"],
         ["???", "There lived a witch named Celestina."],
         ["???", "She lived with her pet bat, Ambrose."],
         ["???", "And she brewed potions for a living."],
-        ["???", "One day as she was brewing a potion, she didn't realize that the window was open and a magical wind current came through the door."],
-        ["???", "Ambrose was whisked away by the wind"],
-        ["???", "Celestina turned around to find that Ambrose was missing."],
+        ["???", "One day as she was brewing a potion, a current of magical wind flew in through the window!"],
+        ["???", "And the wind whisked Ambrose away!"],
+        ["Celestina", "AAAAAAAAAAAAAAAAAAAAA"],
+        ["Celestina", "Ambrose!"],
+        ["???", "Celestina rushed out and searched for hours but Ambrose was nowhere to be found."]
+        ["Celestina", "time to do potion brewing xd haha :D"],
+    ],
+    // meet farmer ivo
+    2 : [
+        ["Farmer Ivo", "Celestina, long time no see!"],
+        ["Celestina", "Farmer Ivo, it's terrible "],
+        ["Farmer Ivo", "Vaht happened?"],
+        ["Celestina", "Ambrose is gone!"],
+        ["Farmer Ivo", "My condolences"],
+        ["Celestina", "!!!"],
+        ["Celestina", "Oh no, Ambrose isn't dead."],
+        ["Celestina", "I'm sure he's having the time of his life!"],
+        ["Celestina", "But I need him to make my potions!"],
+        ["Celestina", "Farmer Ivo, surely you can help! I need some plants from your field."],
+    ],
+    // introduction to puzzle 1
+    3 : [
+
+    ],
+    // meet back with farmer ivo
+    4 : [
+
+    ],
+    // meet idalia
+    5 : [
+
+    ],
+    // dialogue for puzzle 2
+    6 : [
+
+    ],
+    // meet back with idalia
+    7 : [
+
+    ],
+    // meet petrarch
+    8 : [
+
+    ],
+    // puzzle 3
+    9 : [
+
+    ],
+    // meet back with petrarch + finally got everything!
+    10 : [
+
+    ],
+    // use the potion / exeunt
+    11 : [
+
+    ],
+
+    // STRETCH GOALS
+    // in other words bonus dialogue for other places
+    20 : [
+
     ]
+
+
+
+
 } 
 
 // this will be replaced later
