81527f6903b4119a0f7046b7f3898c0ba32f86d7
Highfire1 <tseng.andersonn@gmail.com>
2021-08-04 02:52:17 -0700

    oh god ive been committing with the wrong email address

--- js/dialogue.js
+++ js/dialogue.js
@@ -39,34 +39,6 @@ let strings = {
 
 
 let stringsbad = {
-    // intro sequence
-    "introdialogue" : [
-        ["???", "Once upon a time in a far away land,"],
-        ["???", "There lived a witch named Celestina."],
-        ["???", "She lived with her pet bat, Ambrose."],
-        ["???", "And she brewed potions for a living."],
-        ["???", "One day as she was brewing a potion, a current of magical wind flew in through the window!"],
-        ["???", "And the wind whisked Ambrose away!"],
-        ["Celestina", "AAAAAAAAAAAAAAAAAAAAA"],
-        ["Celestina", "Ambrose!"],
-        ["???", "Celestina rushed out and searched for hours but Ambrose was nowhere to be found."],
-        ["Celestina", "time to do potion brewing xd haha :D"],
-    ],
-    // meet farmer ivo
-    2 : [
-        ["Farmer Ivo", "Celestina, long time no see!"],
-        ["Celestina", "Farmer Ivo, it's terrible "],
-        ["Farmer Ivo", "Vaht happened?"],
-        ["Celestina", "Ambrose is gone!"],
-        ["Farmer Ivo", "My condolences"],
-        ["Celestina", "!!!"],
-        ["Celestina", "Oh no, Ambrose isn't dead."],
-        ["Celestina", "I'm sure he's having the time of his life!"],
-        ["Celestina", "But I need him to make my potions!"],
-        ["Celestina", "Farmer Ivo, surely you can help! I need some plants from your field."],
-        ["Farmer Ivo", "I'm afraid your out of luck."], // MAKE RUSSIAN
-        ["Farmer Ivo", "PUZZLE EXPLANATION"] 
-    ],
     // introduction to puzzle 1
     3 : [
         ["Celestina", "Here goes nothing!"]
